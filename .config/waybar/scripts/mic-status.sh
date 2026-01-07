054447038f0350f9362febdac024028f00935a5c
mafien0 <mafien_0@tuta.io>
2026-01-07 07:51:11 +0500

    even more niri configuration!

--- .config/waybar/scripts/mic-status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-STATUS=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@)
-
-if [[ $STATUS == *"MUTED"* ]]; then
-    echo '{"text": "󰍭", "class": "muted", "tooltip": "Microphone Muted"}'
-else
-    echo '{"text": "󰍬", "class": "unmuted", "tooltip": "Microphone Active"}'
-fi
